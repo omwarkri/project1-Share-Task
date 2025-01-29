5bff98cf0cc97626fdf84dcd7e6df557b3ceec4d
Shantanu <shantanuchavhan10@gmail.com>
2025-01-29 11:49:30 -0800

    commit

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -174,10 +175,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Directory to store static files
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
-# For production: directory to collect static files
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type

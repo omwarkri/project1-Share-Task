20f95ac6bcfc8913846853be541b8d3b5d887b10
Shantanu <shantanuchavhan10@gmail.com>
2025-02-21 05:11:20 -0800

    commit

--- todolist/todolist/asgi.py
+++ todolist/todolist/asgi.py
@@ -1,15 +1,24 @@
+import os
+from django.core.asgi import get_asgi_application
 from django.urls import re_path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.consumers import ChatConsumer
-from django.core.asgi import get_asgi_application
 
+# Set the Django settings module before importing get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolist.settings')
+
+# Django ASGI application to handle HTTP requests
+django_asgi_app = get_asgi_application()
+
+# WebSocket URL routing
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<task_id>\d+)/(?P<receiver_id>\d+)/$", ChatConsumer.as_asgi()),
 ]
-# Make sure HTTP requests are handled correctly
+
+# Define the ASGI application
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  # Fix for handling HTTP requests
+    "http": django_asgi_app,  # Handle HTTP requests
     "websocket": AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),

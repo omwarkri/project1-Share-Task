efa4587e554f8a51852cf21051563fae419aab06
Shantanu <shantanuchavhan10@gmail.com>
2025-02-21 05:35:48 -0800

    commit

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -34,7 +34,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Application definition
 
 INSTALLED_APPS = [
-    "corsheaders"
+    "corsheaders",
     "daphne",
     'django.contrib.admin',
     'django.contrib.auth',

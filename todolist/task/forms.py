1825f403531315346de0348de4a7e589f8fa84b1
Shantanu <shantanuchavhan10@gmail.com>
2025-02-22 14:37:59 -0800

    commit

--- todolist/task/forms.py
+++ todolist/task/forms.py
@@ -6,7 +6,9 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'due_date', 'priority', 'category', 'status', 'dependencies']
-
+        widgets = {
+            'due_date': forms.DateInput(attrs={'type': 'date'}),  # Use HTML5 date input
+        }
     dependencies = forms.ModelMultipleChoiceField(
         queryset=Task.objects.all(), 
         widget=forms.CheckboxSelectMultiple,
